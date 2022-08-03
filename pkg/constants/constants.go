cb170687018fdf01d7b1a05352a666d29f905e10
calvin <wen.chen@daocloud.io>
2022-08-03 19:32:01 +0800

    expose external kubeconfig for apiserver
    Signed-off-by: calvin <wen.chen@daocloud.io>

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -3,6 +3,8 @@ package constants
 const (
 	// Default karmada chart install system.
 	KarmadaNamespace = "karmada-system"
+	// karmada apiserver default port.
+	KarmadaAPIServerNodePort = 32443
 	// karmada chart download dir.
 	ChartBasePath = "/var/run/karmada-operator"
 	// KubeconfigBasePath = "/var/run/karmada-operator/kubeconfig".
---
+++ pkg/utils/cluster.go
@@ -0,0 +1,77 @@
+/*
+Copyright 2022 The Karmada operator Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package utils
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	clientset "k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+// GetCurrentNS fetch namespace the current pod running in.
+// reference to client-go (config *inClusterClientConfig) Namespace() (string, bool, error).
+func GetCurrentNS() (string, error) {
+	if ns := os.Getenv("POD_NAMESPACE"); ns != "" {
+		return ns, nil
+	}
+
+	if data, err := ioutil.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/namespace"); err == nil {
+		if ns := strings.TrimSpace(string(data)); len(ns) > 0 {
+			return ns, nil
+		}
+	}
+	return "", fmt.Errorf("can not get namespace where pods running in")
+}
+
+func GetCurrentNSOrDefault() string {
+	ns, err := GetCurrentNS()
+	if err != nil {
+		return "default"
+	}
+	return ns
+}
+
+func GetKubeMasterIP(client kubernetes.Interface) ([]string, error) {
+	nodeClient := client.CoreV1().Nodes()
+	masterNodes, err := nodeClient.List(context.TODO(), metav1.ListOptions{LabelSelector: "node-role.kubernetes.io/master"})
+	if err != nil {
+		return nil, err
+	}
+
+	masterIPs := make([]string, 0, len(masterNodes.Items))
+	for _, v := range masterNodes.Items {
+		masterIPs = append(masterIPs, v.Status.Addresses[0].Address)
+	}
+
+	return masterIPs, nil
+}
+
+func NewClientForKubeconfig(kubeconfig []byte) (*clientset.Clientset, error) {
+	config, err := clientcmd.RESTConfigFromKubeConfig(kubeconfig)
+	if err != nil {
+		return nil, err
+	}
+
+	return clientset.NewForConfig(config)
+}
