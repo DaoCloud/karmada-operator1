286b5aa2f6324edb6c8c9113211b24bcf53f2595
calvin <wen.chen@daocloud.io>
2023-03-08 10:54:17 +0800

    bump version to v0.1.10
    Signed-off-by: calvin <wen.chen@daocloud.io>

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -2,7 +2,7 @@ package constants
 
 const (
 	// Default karmada chart install system.
-	DefaultKarmadaVersion = "v1.4.0"
+	DefaultKarmadaVersion = "v1.5.0"
 	// Default karmada chart install system.
 	KarmadaNamespace = "karmada-system"
 	// karmada apiserver default port.
