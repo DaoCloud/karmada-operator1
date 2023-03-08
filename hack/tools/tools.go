e06addb48fcfc5d397c42bbd498bfcc20a93fa95
calvin <wen.chen@daocloud.io>
2023-03-08 11:30:39 +0800

    init gh-pages branch
    Signed-off-by: calvin <wen.chen@daocloud.io>

--- hack/tools/tools.go
+++
@@ -1,9 +0,0 @@
-//go:build tools
-// +build tools
-
-package tools
-
-import (
-	_ "k8s.io/code-generator"
-	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
-)
--- vendor/github.com/karmada-io/api/cluster/v1alpha1/cluster_helper.go
+++
@@ -1,6 +0,0 @@
-package v1alpha1
-
-// String returns a well-formatted string for the Cluster object.
-func (c *Cluster) String() string {
-	return c.Name
-}
